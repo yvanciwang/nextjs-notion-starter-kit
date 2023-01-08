a70984e3e4b507008bba206136c1b8d421e314ed
yvanciwang <59338787+yvanciwang@users.noreply.github.com>
2023-01-08 10:17:53 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '1ab80f8640ed41c2be6a5a2367ea5b10',
+  rootNotionPageId: '0da87895d3724c228f286cacbbf3fb28',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
